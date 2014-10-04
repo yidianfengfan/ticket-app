e340fe74de12d4426eb981c02ae20ef1772c07e6
yidianfengfan <yidianfengfan@gmail.com>
2014-10-04 14:30:03 +0800

    Update config.js

--- cloud/config.js
+++ cloud/config.js
@@ -2,8 +2,8 @@
  * Created by lzw on 14-8-19.
  */
 //我申请的一个应用的appId,appKey，你可以替换成自己的，新建一条工单，便可以看到相应的表
-exports.applicationId='my7clww2hnrairocm6uwus69fg806z50vvmtaz7oughnfq94';
-exports.applicationKey='npn86d4vj3ttgl0qemc972v70wrs1oimye04w2x3sb4r0zl7';
+exports.applicationId='rcyc8e0kg2pn3gcqlvctgmrwztkuwsat9dvyay0bsbzr8he1';
+exports.applicationKey='4qpz4cv388iokx8t8ahety5pnxc95zrq9jpenxbqtdbl3wmz';
 
 //express用做加密token的salt，自己申请应用搭建时，可稍微更改字符串，可以更安全
 exports.cookieParserSalt="klp4e8b4sddjp2";
@@ -15,7 +15,7 @@ exports.mailGunKey='';//please use your mailGunKey
 exports.slackUrl='https://avoscloud.slack.com/services/hooks/incoming-webhook?token=rNDqBLRC8TlG4YkPKBZSe2qB';
 
 //配置自己申请的子域名
-exports.hostUrl='http://myticket.avosapps.com';
+exports.hostUrl='http://shguo.avosapps.com';
 
 //邮件的发送者，平时管理员回复了你的工单的时候，你便会收到来自这个邮箱的email
 exports.emailHost='notification@avoscloud.com';
